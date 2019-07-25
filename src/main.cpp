6d0dd6367cfb2ebd0747a5d570c3bca34eb52f49
BrunoCaputo <aluno@local.inatel.br>
2019-07-25 19:11:36 -0300

    Added opengl external libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
 
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
 int main(int argc, char** argv) {
+	if(!glfwInit())
+		return -1;
+
+	// Setup OpenGL context	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	// Create window
+	GLFWwindow * window = glfwCreateWindow(800, 600, "Teste", nullptr, nullptr);
+	
+	if(window == nullptr) {
+		glfwTerminate();
+		return -1;
+	}
+	
+	// Setup window context
+	glfwMakeContextCurrent(window);
+	
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		glfwTerminate();
+		return -1;
+	}
+	
+	while(!glfwWindowShouldClose(window)) {
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	
+	glfwTerminate();
+	
 	return 0;
 }
